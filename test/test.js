c7fb8157d3185d58a9d154ad47153759ff07fa66
Antoine Decriem <decriem.antoine@gmail.com>
2015-11-09 15:59:48 +0100

    test histoire 1

--- test/test.js
+++ test/test.js
@@ -3,4 +3,9 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var moteur = new Engine();
+
+    moteur.nouvelle_partie();
+
+    assertTrue(juxtaposition_ok() === true);
 };
