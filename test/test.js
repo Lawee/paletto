9066922d2a48c2e5d62a9261fe6b8e165881117c
Antoine Decriem <decriem.antoine@gmail.com>
2015-11-12 10:46:54 +0100

    test histoire 3

--- test/test.js
+++ test/test.js
@@ -19,3 +19,16 @@ PalettoTestCase.prototype.testStory2 = function () {
 
     assertTrue(moteur.est_dans_un_coin("jaune") === true);
 };
+
+PalettoTestCase.prototype.testStory2 = function () {
+    var moteur = new Engine();
+
+    moteur.nouvelle_partie();
+
+    moteur.retirer_piece("A6");
+
+    assertTrue(moteur.get_nombre_pieces() === 35);
+
+    var piecesJoueur1 = moteur.get_pieces_joueur(1);
+    assertTrue(piecesJoueur1["jaune"] === 1);
+};
